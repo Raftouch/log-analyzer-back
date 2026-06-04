09ee8bde572aa9dc3c02d3569527ea41001ae617
raftouch <rafanelkin@gmail.com>
2026-06-04 15:55:00 +0200

    refactor(logs): introduce controller and route separation for application logs

--- app.js
+++ app.js
@@ -5,26 +5,20 @@ dotenv.config();
 const port = process.env.PORT || 4000;
 
 const parseLogs = require("./src/parser");
-const applicationLogs = parseLogs("./logs/application.log");
 const systemLogs = parseLogs("./logs/system.log");
-// console.log("Application logs : ", applicationLogs);
 // console.log("System logs : ", systemLogs);
 
 const analyzeLogs = require("./src/analyzer");
-const analyzedApplicationLogs = analyzeLogs(applicationLogs);
 const analyzedSystemLogs = analyzeLogs(systemLogs);
-// console.log("Analyzed application logs : ", analyzedApplicationLogs);
 // console.log("Analyzed system logs : ", analyzedSystemLogs);
 
 const getSeverityLevel = require("./src/severity");
-console.log(
-  "Severity for application logs : ",
-  getSeverityLevel(analyzedApplicationLogs),
-);
-console.log(
-  "Severity for system logs : ",
-  getSeverityLevel(analyzedSystemLogs),
-);
+// console.log(
+//   "Severity for system logs : ",
+//   getSeverityLevel(analyzedSystemLogs),
+// );
+
+app.use("/logs/application", require("./routes/application"));
 
 const start = () => {
   try {
