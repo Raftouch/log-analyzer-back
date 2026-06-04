940385ec1f9655f02d9517bfe53f9426df512e6f
raftouch <rafanelkin@gmail.com>
2026-06-04 14:18:48 +0200

    test(logs): run parser on sample log files

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 4000;
 
+const parseLogs = require("./src/parser");
+const applicationLogs = parseLogs("./logs/application.log");
+const systemLogs = parseLogs("./logs/system.log");
+console.log("Application logs : ", applicationLogs);
+console.log("System logs : ", systemLogs);
+
 const start = () => {
   try {
     app.listen(port, () => console.log(`App listening on port ${port}`));
